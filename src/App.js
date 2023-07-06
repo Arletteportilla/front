fab281690740d13e4b2994e5b66a14d7edac7212
Arlette <arportillaca@uide.edu.ec>
2023-07-05 20:03:20 -0500

    React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Input,InputAdornment, ListItemIcon, ListItemText, MenuItem, MenuList } from '@mui/material';
+import InicioIcon from '@mui/icons-material/Home';
+import TareasIcon from '@mui/icons-material/Book';
+import CorreosIcon from '@mui/icons-material/Email';
+import BusquedaIcon from '@mui/icons-material/Search';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+        <MenuList>
+          <MenuItem>
+          <ListItemIcon><InicioIcon color='success'></InicioIcon></ListItemIcon>
+          <ListItemText>Inicio</ListItemText>
+          </MenuItem>
+          <MenuItem>
+          <ListItemIcon><TareasIcon color='error'></TareasIcon></ListItemIcon>
+          <ListItemText>Tareas</ListItemText>
+          </MenuItem>
+          <MenuItem>
+          <ListItemIcon><CorreosIcon color='secondary'></CorreosIcon></ListItemIcon>
+          <ListItemText>Correos</ListItemText>
+          </MenuItem>
+        </MenuList>
+        </div>
+        <div>
+        <Input id="input-with-icon-adornment"
+          startAdornment={
+            <InputAdornment position="Start">
+            <BusquedaIcon></BusquedaIcon>
+            </InputAdornment>
+          }
+          />
+        </div>
       </header>
+
     </div>
   );
 }
